4c1bc9c8f671de8fa0e7414cb158614881e63626
Trần Thị Thúy <tranthuy0611bacninh@gmail.com>
2022-10-05 18:05:47 +0700

    update pagination

--- BT-ajax-pokemon/main.js
+++ BT-ajax-pokemon/main.js
@@ -1,30 +1,28 @@
 class Pokemon{
     constructor(){
         this.limit = 100
-        this.offset = 100
-        this.offsetUnit = 100
+        this.offset = 0
+        this.unitSetOffset = 100
+        this.urlNext = ''
+        this.urlPrevious = ''
         this.pokemonList = []
         this.pokemonListTable = document.querySelector('.pokemon-table')
         this.preBtn = document.querySelector('.pre-btn')
         this.nextBtn = document.querySelector('.next-btn')
     }
-    getData(limit, offset){
-        axios.get('https://pokeapi.co/api/v2/pokemon', {
+    async getData(limit, offset){
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon', {
             params: {
                 limit,
                 offset
             }
         })
-        .then(res => {
-            let {results} = res.data
-            this.pokemonList = results
-            this.displayPokemonList()
-            return results
-        })
+        .then(res => res)
         .catch(err => {
             console.log(err)
             return []
         })
+        return response 
     }
     displayPokemonList(){
         let htmls = ''
@@ -39,25 +37,39 @@ class Pokemon{
         })
         this.pokemonListTable.innerHTML = htmls
     }
-    handlePreviousBtn(){
-        if(this.offset == 100){
-            this.preBtn.classList.add('disable')
+    handlePaginations(button){
+        let url = ''
+        switch (button.className) {
+            case 'next-btn' : 
+               url = this.urlNext
+               this.unitSetOffset = Math.abs(this.unitSetOffset)
+               break
+            case 'pre-btn' :
+                url = this.urlPrevious
+                this.unitSetOffset = -Math.abs(this.unitSetOffset)
+                break
+        }
+        if(url){
+            button.disabled = false
+            this.offset += this.unitSetOffset
+            this.getData(this.limit,this.offset).then(res => this.handleDataFromAPI(res))
         } else {
-            this.preBtn.classList.remove('disable')
-            this.offset -= this.offsetUnit
-            this.getData(limit,this.offset)
+            button.disabled = true
         }
     }
-    handleNextBtn(){
-        this.offset += this.offsetUnit
-        this.getData(limit,this.offset)
-    }
     handleEvent(){
-        this.nextBtn.onclick = () => {this.handleNextBtn()}
-        this.preBtn.onclick = () => {this.handlePreviousBtn()}
+        this.nextBtn.onclick = (e) => {this.handlePaginations(e.target)}
+        this.preBtn.onclick = (e) => {this.handlePaginations(e.target)}
+    }
+    handleDataFromAPI(res){
+        let {results, next, previous} = res.data
+        this.urlNext = next
+        this.urlPrevious = previous
+        this.pokemonList = results
+        this.displayPokemonList()
     }
     init() {
-        this.getData(this.limit,this.offset)
+        this.getData(this.limit,this.offset).then(res => this.handleDataFromAPI(res))
         this.handleEvent()
     }
 }
